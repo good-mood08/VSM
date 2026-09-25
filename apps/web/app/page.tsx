294a33e4cdf41b8e1c30dfd8bda79a1ce739c358
Mark <mega.nemirovskiy@gmail.com>
2026-09-25 18:20:15 +0300

    commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home(){
     <div className="py-20.5 px-7 flex flex-col gap-10">
       <div className="flex flex-row justify-between">
         <h1 className="text-accent text-[26px] font-ru-rail">Добрый день,<br/> {name}!</h1>
+        
         {!newMessages ? (
           <Image 
             src='/svg/empty-notification.svg'
